dad7b15213e1dc0d22fd2a9aeb6c585c0ff80294
Duane Northcutt <duane.northcutt@gmail.com>
2024-08-15 15:27:22 -0700

    WIP. Cleaned up.

--- test/shotTest/shotTest.ino
+++ test/shotTest/shotTest.ino
@@ -10,24 +10,25 @@
 #include <OnBoardLED.h>
 
 
-#define PWM_PIN     D3    // EN
-#define DIR_PIN     D2    // PH
-#define _SLEEP_PIN  D1    // _SLEEP
-#define CS_PIN      A0    // CS
-#define _FAULT_PIN  D10   // _FAULT
-#define TRIG_PIN    D9
+#define PWM_PIN         D3    // EN
+#define DIR_PIN         D2    // PH
+#define _SLEEP_PIN      D1    // _SLEEP
+#define CS_PIN          A0    // CS
+#define _FAULT_PIN      D10   // _FAULT
+#define TRIG_PIN        D9
+
+#define MIN_FREQ        500   // TODO fix this
+#define MAX_FREQ        1000  // TODO fix this
+
+#define MIN_SHOT_RATE   1000  // 1 shot/sec
+#define MAX_SHOT_RATE   150   // 7 shots/sec = 143msec
 
 #define MIN_DUTY_CYCLE  11.0
 #define MAX_DUTY_CYCLE  99.9
-#define MIN_FREQ        500
-
-#define FEEDER_DUTY_CYCLE MAX_DUTY_CYCLE
-#define MIN_FEEDER_DUTY_CYCLE 25.0
-
-#define TRIGGER_TIME    150  // 7 shots/sec = 143msec
+#define MIN_FEEDER_TIME 1000  // msec
 
 #define PRIME_PELLETS   32   // a function of the length of the feeder tube
-#define PRIME_TIME      750  // time to prime the feeder line (@ max speed) TMP TMP TMP
+//#define PRIME_TIME      750  // time to prime the feeder line (@ max speed) TMP TMP TMP
 
 #define CMD_Q_SIZE      8
 
@@ -35,24 +36,24 @@
 typedef enum CmdType_e {
   FIRE,
   CLEAR,
+  FEED,
+  TRIGGER,
   STOP
 } CmdType;
 
 typedef struct {
   CmdType cmd;
   uint32_t numShots;
-  float shotRate;  // shots/sec
+  union {
+    float shotRate;  // shots/sec
+    float dutyCycle;
+  } data;
 } ShotCmd_t;
 
 
-float freq;
-float dutyCycle;
-unsigned long lastTime;
-unsigned int numShots;
 bool primed;
-unsigned long feederTime;
+float freq;
 unsigned long triggerTime;
-unsigned long loopCnt = 0;
 
 RP2040_PWM *PWM_Instance;
 
@@ -69,39 +70,47 @@ constexpr void bar(int foo) {
 }
 */
 
+// normalize feeder duty cycle value
+constexpr float normDutyCycle(float dutyCycle) {
+  dutyCycle = (((dutyCycle > 0) && (dutyCycle < MIN_DUTY_CYCLE)) ? MIN_DUTY_CYCLE : dutyCycle);
+  dutyCycle = ((dutyCycle > MAX_DUTY_CYCLE) ? MAX_DUTY_CYCLE : dutyCycle);
+  return dutyCycle;
+}
+
 void primeFeeder(int numPellets) {
-  Serial.println("p");
-  PWM_Instance->setPWM(PWM_PIN, freq, FEEDER_DUTY_CYCLE);
-  delay(PRIME_TIME);  //// FIXME compute delay based on number of pellets
+  Serial.print("p");Serial.println(numPellets);
+  PWM_Instance->setPWM(PWM_PIN, freq, MAX_DUTY_CYCLE);
+//  delay(numPellets * <MIN_MSEC_PER_PELLET>);
+  delay(3000);
   PWM_Instance->setPWM(PWM_PIN, freq, 0.0);
 }
 
-void fire(int numPellets, float pelletRate) {
+void fire(int numShots, float shotRate) {
   if (primed == false) {
     primeFeeder(PRIME_PELLETS);
     primed = true;
   }
 
-  //// TODO deal with min feed speed
-  uint32_t time = ((numPellets / pelletRate) * 1000.0);
-  startTrigger(numPellets, time);
-}
+  shotRate = ((shotRate < MIN_SHOT_RATE) ? MIN_SHOT_RATE : shotRate);
+  shotRate = ((shotRate > MAX_SHOT_RATE) ? MAX_SHOT_RATE : shotRate);
 
-void stop() {
-  stopFeeder();
-  stopTrigger();
+  uint32_t duration = ((numShots / shotRate) * 1000.0);
+  //// TODO deal with min feed speed
+//  duration = ((duration < MIN_SHOT_TIME) ? MIN_SHOT_TIME : duration);
+  startTrigger(numShots, duration);
 }
 
 void startFeeder(uint32_t shots, uint32_t duration) {
-  //// TODO deal with min feeder speed
   unsigned long now = millis();
-  feederTime = now + ((duration < MIN_FEEDER_DUTY_CYCLE) ? MIN_FEEDER_DUTY_CYCLE : duration);
-  PWM_Instance->setPWM(PWM_PIN, freq, MAX_DUTY_CYCLE);  //// FIXME select proper speed
+  //// FIXME select proper speed to do that many shots in the given time
+//  float dutyCycle = normDutyCycle(????);
+  float dutyCycle = MAX_DUTY_CYCLE;
+  PWM_Instance->setPWM(PWM_PIN, freq, normDutyCycle(dutyCycle));
 }
 
 void stopFeeder() {
+  Serial.println("stopFeeder");
   PWM_Instance->setPWM(PWM_PIN, freq, 0.0);
-  feederTime = 0;
 }
 
 void startTrigger(uint32_t shots, uint32_t duration) {
@@ -114,6 +123,7 @@ void startTrigger(uint32_t shots, uint32_t duration) {
 }
 
 void stopTrigger() {
+  Serial.println("stopTrigger");
   digitalWrite(TRIG_PIN, LOW);
   triggerTime = 0;
   stopFeeder();
@@ -142,37 +152,39 @@ void getInput(cppQueue *qPtr) {
     case 'C':
     case 'c':
       cmd.cmd = CLEAR;
-      qPtr->push(&cmd);
       break;
     case 'f':
       Serial.print("Fire: ");
       cmd.cmd = FIRE;
       //// FIXME get args from console input
       cmd.numShots = Serial.parseFloat();
-      cmd.shotRate = Serial.parseInt();
-      Serial.print(cmd.numShots);Serial.print(", ");Serial.println(cmd.shotRate);
-      qPtr->push(&cmd);
+      cmd.data.shotRate = Serial.parseInt();
+      Serial.print(cmd.numShots);Serial.print(", ");Serial.println(cmd.data.shotRate);Serial.flush();
       break;
     case 'o':
       Serial.println("Continuous");
       //// FIXME figure this one out
       cmd.cmd = FIRE;
       cmd.numShots = 1000000;
-      cmd.shotRate = 5.0;  //// TODO figure out appropriate rate
-      qPtr->push(&cmd);
+      cmd.data.shotRate = 5.0;  //// TODO figure out appropriate rate
+      break;
+    case 'p':
+      Serial.println("Turn Feeder On");
+      cmd.cmd = FEED;
+      cmd.data.dutyCycle = Serial.parseFloat();
       break;
     case 's':
-      Serial.println("Single Shot");
+      Serial.println("Single Shot");Serial.flush();
       cmd.cmd = FIRE;
       cmd.numShots = 1;
-      cmd.shotRate = 1.0;  //// TODO figure out appropriate rate
-      qPtr->push(&cmd);
+      cmd.data.shotRate = 1.0;  //// TODO figure out appropriate rate
       break;
     default:
       Serial.println("Stop Shooting");
       cmd.cmd = STOP;
-      qPtr->push(&cmd);
+      break;
     }
+    qPtr->push(&cmd);
 
     // flush the input
     while (Serial.available()) {
@@ -186,12 +198,8 @@ void setup() {
   while (!Serial) { ; };
   Serial.println("BEGIN");
 
-  freq = 2000.0;
-  dutyCycle = FEEDER_DUTY_CYCLE;
-  lastTime = millis();
-  numShots = 0;
+  freq = 2000.0;  //// TODO figure out the right value for this
   primed = false;
-  feederTime = 0;
   triggerTime = 0;
 
   // set up feeder motor controller
@@ -213,12 +221,13 @@ void setup() {
   neoPix = new OnBoardLED(NEOPIXEL_POWER, PIN_NEOPIXEL);
   neoPix->setColor(BLACK);
 
-  Serial.println("START");
+  Serial.println("START");Serial.flush();
 }
 
 void loop() {
   getInput(&cmdQ);
   if (cmdQ.isEmpty() == false) {
+    float dutyCycle;
     ShotCmd_t cmd;
     cmdQ.pop(&cmd);
     switch (cmd.cmd) {
@@ -226,72 +235,39 @@ void loop() {
       clearFeeder();
       break;
     case FIRE:
-      fire(cmd.numShots, cmd.shotRate);
+      fire(cmd.numShots, cmd.data.shotRate);
+      break;
+    case FEED:
+      dutyCycle = normDutyCycle(cmd.data.dutyCycle);
+      PWM_Instance->setPWM(PWM_PIN, freq, dutyCycle);
       break;
     case STOP:
-      stop();
+      stopFeeder();
+      stopTrigger();
+      break;
     default:
-      Serial.print("ERROR: invalid command type - "); Serial.println(cmd.cmd);
+      Serial.print("ERROR: invalid command type ("); Serial.print(cmd.cmd);Serial.println(")");
       break;
     }
   }
 
   unsigned long now = millis();  // N.B. rolls over after ~50 days of uptime
   //// TODO deal with rollover
-  if (feederTime && (feederTime <= now)) {
-    stopFeeder();
-  }
   if (triggerTime && (triggerTime <= now)) {
+    Serial.print(now);Serial.println(triggerTime);
     stopTrigger();
   }
 
   if (digitalRead(_FAULT_PIN) == 0) {
-    if (dutyCycle > MIN_DUTY_CYCLE) {
-      neoPix->setColor(RED);
+    if (triggerTime) {
+      neoPix->setColor(RED);  // fault while active
       digitalWrite(_SLEEP_PIN, LOW);
       delay(5);
       digitalWrite(_SLEEP_PIN, HIGH);
     } else {
-      neoPix->setColor(MAGENTA);
+      neoPix->setColor(MAGENTA);  // no fault while active
     }
   } else {
-    neoPix->setColor(BLACK);
+    neoPix->setColor(BLACK);  // no fault
   }
-
-  loopCnt++;
 }
-
-    /*
-    case 'd':
-      dutyCycle = Serial.parseFloat();
-      if (dutyCycle < MIN_DUTY_CYCLE) {
-        dutyCycle = 0.0;
-      } else if (dutyCycle >= 100.0) {
-        dutyCycle = MAX_DUTY_CYCLE;
-      }
-      Serial.print("Speed: "); Serial.println(dutyCycle);
-      break;
-    */
-
-  /*
-  if (numShots > 0) {
-    if (primed == false) {
-      primeFeeder(PRIME_PELLETS);
-      primed = true;
-    }
-    digitalWrite(TRIG_PIN, HIGH);  // pull trigger
-    neoPix->setColor(GREEN);
-    PWM_Instance->setPWM(PWM_PIN, freq, dutyCycle);  // start feeder
-    delay(TRIGGER_TIME);
-    numShots--;
-    if (numShots > 0) {
-      Serial.println("+");
-    } else {
-      Serial.println("-");
-    }
-  } else {
-    PWM_Instance->setPWM(PWM_PIN, freq, 0.0);  // stop feeder
-    digitalWrite(TRIG_PIN, LOW);  // release trigger
-    neoPix->setColor(BLACK);
-  }
-  */
